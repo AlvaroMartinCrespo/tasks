05699e77ef799a176792c5cd75836ec88412ee40
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-12-15 13:53:42 +0100

    session

--- src/components/Tasks/tasks.tsx
+++ src/components/Tasks/tasks.tsx
@@ -1,12 +1,14 @@
-function Tasks({ session }) {
+import { Session } from '@supabase/supabase-js';
+
+function Tasks({ session }: { session: Session }) {
   return (
     <>
       <div className="p-5 m-5 border absolute">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => console.log(session)}
+          onClick={() => console.log(session.user)}
         >
-          Imprime sesion
+          Imprime User
         </button>
       </div>
     </>
